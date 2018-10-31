0171be6f305213455aa358cd5669dd0b594b024f
OttoSteel <polshinski@gmail.com>
2018-10-31 23:02:14 +0200

    October31year2018v2

---
+++ 07_complex_conditions/js/script.js
@@ -0,0 +1,10 @@
+console.log('You are at ' + window.location);
+
+let night = true;
+let tired = true;
+
+if(night || tired) {
+	console.log('relax, bro!');
+} else {
+	console.log('code a little bit');
+}
